88b410162cd78942d8068dd0fe686d37a3a97752
Ryan W. Anderson <anderson.ryan.w@live.com>
2025-04-14 18:58:20 -0500

    Moved spacetimedb.nix to default

--- default.nix
+++ default.nix
@@ -1,7 +1,34 @@
-let 
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
 {
-  spacetimedb = pkgs.callPackage ./spacetimedb.nix {};
+  stdenv,
+  lib,
+  fetchurl,
+  autoPatchelfHook,
+  libgcc,
+}:
+stdenv.mkDerivation {
+  pname = "spacetimedb";
+  version = "1.0.1";
+  src = fetchurl {
+    url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.1/spacetime-x86_64-unknown-linux-gnu.tar.gz";
+    hash = "sha256-dHxb9hNbQwfww9CJUp6IE4etxro9qvFQGyHOhJMYkV0=";
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  buildInputs = [
+    libgcc
+    stdenv.cc.cc.lib
+  ];
+
+  unpackPhase = ''
+    runHook preUnpack
+    mkdir -p $out/bin
+    tar -xvf $src -C $out/bin
+    runHook postUnpack
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    runHook postInstall
+  '';
 }
