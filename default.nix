0a2ab1a11b7d4e756716b60650ae772ac5d5476e
Ryan W. Anderson <anderson.ryan.w@live.com>
2025-04-16 00:46:10 -0500

    Updated to version 1.1.0

--- default.nix
+++ default.nix
@@ -12,8 +12,8 @@ stdenv.mkDerivation {
   pname = "spacetimedb";
   version = "1.0.1";
   src = fetchurl {
-    url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.0.1/spacetime-x86_64-unknown-linux-gnu.tar.gz";
-    hash = "sha256-dHxb9hNbQwfww9CJUp6IE4etxro9qvFQGyHOhJMYkV0=";
+    url = "https://github.com/clockworklabs/SpacetimeDB/releases/download/v1.1.0/spacetime-x86_64-unknown-linux-gnu.tar.gz";
+    hash = "sha256-t7V4TkPWQfzH8PUkRLq1OMaKhZ4in4EZCLKinc2oSik=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
